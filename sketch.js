2904b41d580dde9cf68dc4f8df5a707f35c6d80f
jmcox229 <97695174+jmcox229@users.noreply.github.com>
2026-01-14 07:02:37 +0000

    attempting to stage changes in sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,15 @@
-console.log("sketch.js is running");
+
+let bottomImg, topImg;
+
+function preload() {
+  bottomImg = loadImage('assets/images/bear-knight.jpeg');
+  topImg = loadImage('assets/images/dog.jpeg');
+}
+function setup() {
+  createCanvas(720, 400);
+  image(topImg, 0, 0);
+}
+
+function mouseDragged() {
+  copy(bottomImg, mouseX, mouseY, 20, 20, mouseX, mouseY, 20, 20);
+}
